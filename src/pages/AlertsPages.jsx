9a32f78d72ad216b02cc1371c9d9bbde5874bca5
pinflow1 <eonu4112@gmail.com>
2026-05-30 15:04:26 +0100

    Create AlertsPages.jsx

---
+++ src/pages/AlertsPages.jsx
@@ -0,0 +1,12 @@
+import DrippingS from "../components/DrippingS"
+
+export default function AlertsPage() {
+  return (
+    <div style={{ padding: "80px 16px", textAlign: "center" }}>
+      <DrippingS size={36} color="#222" />
+      <p style={{ fontSize: 15, color: "#333", fontFamily: "'DM Sans', sans-serif", marginTop: 16 }}>
+        Alerts coming soon.
+      </p>
+    </div>
+  )
+}
